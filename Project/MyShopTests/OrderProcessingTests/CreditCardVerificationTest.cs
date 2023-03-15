30cc62bd3bf04a3ead2685366674576293bea3dd
Anaiya798 <persikk3000@gmail.com>
2023-03-15 16:47:54 +0300

    update tests

---
+++ Project/MyShopTests/OrderProcessingTests/CreditCardVerificationTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Shop.CustomerDataProcessing;
+using Shop.Data.Models;
+namespace ShopTests.OrderProcessingTests
+
+{
+    [TestFixture]
+    public class CreditCardVerificationTests
+    {
+        private CreditCardChecker _creditCardChecker;
+        private Order _order;
+        [SetUp]
+
+        public void Setup()
+        {
+            _creditCardChecker = new CreditCardChecker();
+            _order = new Order();
+        }
+
+        [Test]
+        public void IncorrectCreditCardTest()
+        {
+            _order.CreditCardNumber = "";
+            Assert.IsFalse(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "#!@80asa";
+            Assert.IsFalse(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "45612612123454679900";
+            Assert.IsFalse(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "456126121";
+            Assert.IsFalse(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "4561261212345464";
+            Assert.IsFalse(_creditCardChecker.Check(_order));
+        }
+
+        [Test]
+        public void СorrectCreditCardTest()
+        {
+            _order.CreditCardNumber = "4561261212345467";
+            Assert.IsTrue(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "0531261212345464";
+            Assert.IsTrue(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "0000000000000000";
+            Assert.IsTrue(_creditCardChecker.Check(_order));
+            _order.CreditCardNumber = "0100000000000009";
+            Assert.IsTrue(_creditCardChecker.Check(_order));
+        }
+    }
+}
---
+++ Project/MyShopTests/OrderProcessingTests/PhoneVerificationTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Shop.CustomerDataProcessing;
+using Shop.Data.Models;
+namespace ShopTests.OrderProcessingTests
+
+{
+    [TestFixture]
+    public class PhoneVerificationTests
+    {
+        private PhoneChecker _phoneChecker;
+        private Order _order;
+        [SetUp]
+
+        public void Setup()
+        {
+            _phoneChecker = new PhoneChecker();
+            _order = new Order();
+        }
+
+        [Test]
+        public void IncorrectPhoneTest()
+        {
+            _order.Phone = "";
+            Assert.IsFalse(_phoneChecker.Check(_order));
+            _order.Phone = "9999999999999999999999";
+            Assert.IsFalse(_phoneChecker.Check(_order));
+        }
+
+        [Test]
+        public void ÑorrectPhoneTest()
+        {
+            _order.Phone = "+79117000000";
+            Assert.IsTrue(_phoneChecker.Check(_order));
+            _order.Phone = "+ 374 91 972459";
+            Assert.IsTrue(_phoneChecker.Check(_order));
+        }
+    }
+}
