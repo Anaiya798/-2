2c00c778fe6068101ac9bbf8702bd4ba5ef2553f
Anastasiia Isakova <persikk3000@gmail.com>
2023-03-18 01:03:40 +0300

    update tests

---
+++ Project/MyShopTests/DatabaseTests/GetReagentByIdTests.cs
@@ -0,0 +1,57 @@
+﻿using Shop.Database;
+using Shop.Models;
+
+namespace MyShopTests.DatabaseTests
+{
+    [TestFixture]
+    public class GetReagentByIdTests
+    {
+        private AppDBContent _dbStub;
+        private ReagentsRepositiry _reagentsRepository;
+        private List<Reagent> _reagents;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbStub = new AppDBContent();
+
+            _reagents = new List<Reagent>() {
+                new Reagent() { Name = "Formic acid" , Category = 1},
+                new Reagent() { Name = "Ethanol", Category = 1 }
+             };
+     
+
+            _dbStub.Reagent.AddRange(_reagents);
+            _dbStub.SaveChanges();
+
+            _reagentsRepository = new ReagentsRepositiry(_dbStub);
+        }
+
+        [Test]
+        public void CorrectIdTest()
+        {
+            var desiredReagent = _reagentsRepository.getObjectReagent(1);
+            Assert.That(desiredReagent.Name, Is.EqualTo(_reagents[0].Name));
+            Assert.That(desiredReagent.Category, Is.EqualTo(_reagents[0].Category));
+
+            desiredReagent = _reagentsRepository.getObjectReagent(2);
+            Assert.That(desiredReagent.Name, Is.EqualTo(_reagents[1].Name));
+            Assert.That(desiredReagent.Category, Is.EqualTo(_reagents[1].Category));
+        }
+
+        [Test]
+        public void NonExistentIdTest()
+        {
+            var desiredReagent = _reagentsRepository.getObjectReagent(3);
+            Assert.Null(desiredReagent);
+
+            desiredReagent = _reagentsRepository.getObjectReagent(15);
+            Assert.Null(desiredReagent);
+
+            desiredReagent = _reagentsRepository.getObjectReagent(-100);
+            Assert.Null(desiredReagent);
+
+        }
+
+    }
+}
