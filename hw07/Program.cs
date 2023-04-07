ad06eec9b84a3c930a8a7429127f0874c549f53e
Anastasiia Isakova <persikk3000@gmail.com>
2023-04-07 16:28:15 +0300

    add hw7

---
+++ hw07/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EF
+{
+    public class User
+    {  
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=users.db");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                
+                User tom = new User { Name = "Tom", Age = 33 };
+                User alice = new User { Name = "Alice", Age = 26 };
+
+   
+                db.Users.Add(tom);
+                db.Users.Add(alice);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+    }
+}
