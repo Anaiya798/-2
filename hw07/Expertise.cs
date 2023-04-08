9b8393e575ee0f6ce0f16c54bd27beac1952df07
Anastasiia Isakova <persikk3000@gmail.com>
2023-04-09 02:17:22 +0300

    fix domen in hw07

---
+++ hw07/Expertise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace EF
+{
+    [Table("Expertise")]
+    public class Expertise
+    { 
+        [Key]
+        public int Id { get; set;}     
+        [Required]
+        public int ReagentID { get; set; }
+        [StringLength(20)]
+        [Required]
+        public string Toxicity { get; set; }
+        [StringLength(20)]
+        [Required]
+        public string WaterSolubility { get; set; }
+    }
+}
---
+++ hw07/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace EF
+{
+    
+    public class WorkingContext : DbContext
+    {
+        public WorkingContext(): base("ChemStat")
+        { }
+
+        public DbSet<Reagent> Reagents { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Producer_Category> ProducerCategory { get; set; }
+        public DbSet<Expertise> Expertises { get; set; }
+
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Category category1 = new Category { Name = "Inorganic", NumberOfReagents = 0 };
+            CategoryFunctions.DeleteCategory(1);
+           
+            //CategoryFunctions.AddCategory(category1);
+            /*using (WorkingContext db = new WorkingContext())
+            {
+                // создаем два объекта User
+                Category category1 = new Category { Name = "Inorganic" , NumberOfReagents = 0};
+                
+
+                // добавляем их в бд
+                db.Categories.Add(category1);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var categories = db.Categories;
+                Console.WriteLine("Список объектов:");
+                foreach (Category u in categories)
+                {
+                    Console.WriteLine("{0}:{1}", u.Id, u.Name);
+                }
+            }*/
+            Console.Read();
+        }
+    }
+}
