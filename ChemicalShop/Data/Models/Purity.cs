18132e4645428f37dc5e7558e85d55707378bd77
Anastasiia Isakova <persikk3000@gmail.com>
2023-04-20 01:17:30 +0300

    Add 1st prototype

---
+++ ChemicalShop/Data/Models/Purity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Data.Models
+{
+    public class Purity
+    {
+        public int Id { get; set; }
+        public string PurityDegree { get; set; }   
+        public List<Reagent> Reagents { get; set; } 
+    }
+}
---
+++ ChemicalShop/Data/Models/Reagent.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.Data.Models
+{
+    public class Reagent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public int CategoryId  { get; set; }
+        public int PurityId { get; set; }   
+        public ushort Price { get; set; }   
+        public string Img { get; set; } 
+        public bool isFavourite { get; set; }   
+        public bool Available { get; set; } 
+        public virtual Category Category { get; set; }
+        public virtual Purity Purity { get; set; }
+    }
+}
