652b483d87891364d00747d365d4ed114dd158d3
Anastasiia Isakova <persikk3000@gmail.com>
2023-05-08 20:03:27 +0300

    add bootstrap

---
+++ ChemicalShop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCategory : IReagentsCategory
+    {
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category {Name = "Неорганическая химия"},
+                    new Category {Name = "Растворители"}
+                };
+            }
+        }
+    }
+}
