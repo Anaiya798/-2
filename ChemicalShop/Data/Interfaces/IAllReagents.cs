c4471b90f5aa021bb578d33954cd799c78ab7893
Anastasiia Isakova <persikk3000@gmail.com>
2023-05-12 17:46:36 +0300

    add database

---
+++ ChemicalShop/Data/Interfaces/IAllReagents.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.Data.Interfaces
+{
+    public interface IAllReagents
+    {
+        IEnumerable<Reagent> Reagents { get;} 
+        IEnumerable<Reagent> FavReagents { get; } 
+        Reagent getObjectReagent(int reagentId);
+    }
+}
---
+++ ChemicalShop/Data/Repository/ReagentsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ChemicalShop.Data.Repository
+{
+    public class ReagentsRepository : IAllReagents
+    {
+        private readonly AppDbContent _appDbContent;
+
+        public ReagentsRepository(AppDbContent appDbContent)
+        {
+            _appDbContent = appDbContent;
+        }
+        public IEnumerable<Reagent> Reagents => _appDbContent.Reagent.Include(reagent => reagent.Category);
+
+        public IEnumerable<Reagent> FavReagents => _appDbContent.Reagent.Where(reagent => reagent.isFavourite).Include(reagent => reagent.Category);
+        public Reagent getObjectReagent(int reagentId) => _appDbContent.Reagent.FirstOrDefault(reagent => reagent.Id == reagentId);
+       
+    }
+}
