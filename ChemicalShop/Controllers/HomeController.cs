57d74355311f1ed19a8633d32e00baea1966521b
Anastasiia Isakova <persikk3000@gmail.com>
2023-05-15 01:47:39 +0300

    add products categories

---
+++ ChemicalShop/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ChemicalShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+
+namespace ChemicalShop.Controllers
+{
+    public class HomeController: Controller
+    {
+        private readonly IAllReagents _reagentsRepository;
+
+        public HomeController(IAllReagents reagentsRepository, ShopCart shopCart)
+        {
+            _reagentsRepository = reagentsRepository;
+            
+        }
+        
+        public ViewResult Index()
+        {
+            var homeReagents = new HomeViewModel
+            {
+                FavReagents = _reagentsRepository.FavReagents
+            };
+            return View(homeReagents);
+        }
+    }
+}
---
+++ ChemicalShop/ViewModels/ShopCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Data.Models;
+
+namespace ChemicalShop.ViewModels
+{
+    public class ShopCartViewModel
+    {
+        public ShopCart UserShopCart { get; set; }
+    }
+}
